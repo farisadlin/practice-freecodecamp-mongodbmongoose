9231077e1a0a5dd4851ad5259b7291f18abba568
shifair <farisnura@gmail.com>
2021-08-20 00:06:34 +0700

    Answered with a new solution

--- myApp.js
+++ myApp.js
@@ -3,10 +3,19 @@ const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+var Schema = mongoose.Schema;
+
+var personSchema = new Schema({
+  name: { type: String, required: true},
+  age: Number,
+  favoriteFoods: [String],
+});
+
 let Person;
+Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  done(null, Person);
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
